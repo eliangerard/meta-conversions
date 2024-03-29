eddc09d3b9a9d65f6882d1c461a92a434def5281
eliangerard <eliangerardgg@gmail.com>
2024-03-29 10:17:24 -0600

    Agrega soporte para HTTPS en el servidor

--- server.js
+++ server.js
@@ -2,20 +2,22 @@ const express = require('express');
 const cors = require('cors');
 const routes = require('./src/routes/routes');
 const https = require('https');
+const fs = require('fs');
 const app = express();
+const path = require('path');
 
 app.use(express.json());
 app.use(cors());
 
 const ssl = {
-    key: fs.readFileSync('clave-privada.pem'),
-    cert: fs.readFileSync('certificado.pem'),
+    // key: fs.readFileSync('clave-privada.pem'),
+    // cert: fs.readFileSync('certificado.pem'),
 };
 
 const server = https.createServer(ssl, app);
 
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static('public'))
 app.use('/event', routes);
 
 const PORT = process.env.PORT || 3000;
