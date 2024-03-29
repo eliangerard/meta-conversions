27cb163a7e07cf7ee1701f6e73e8e2daabe9c62c
eliangerard <eliangerardgg@gmail.com>
2024-03-29 10:33:41 -0600

    Added ssl

--- server.js
+++ server.js
@@ -10,21 +10,17 @@ app.use(express.json());
 app.use(cors());
 
 const ssl = {
-    // key: fs.readFileSync('clave-privada.pem'),
-    // cert: fs.readFileSync('certificado.pem'),
+    key: fs.readFileSync('privkey.pem'),
+    cert: fs.readFileSync('fullchain.pem'),
 };
 
 const server = https.createServer(ssl, app);
-
 
 app.use(express.static('public'))
 app.use('/event', routes);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Servidor escuchando en http://localhost:${PORT}`);
+server.listen(PORT, () => {
+    console.log(`Servidor escuchando en https://localhost:${port}`);
 });
-// server.listen(PORT, () => {
-//     console.log(`Servidor escuchando en https://localhost:${port}`);
-// });
