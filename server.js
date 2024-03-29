2d8a9b2acb6199f8972a7bb7327d468a78f5c30f
eliangerard <eliangerardgg@gmail.com>
2024-03-29 10:08:27 -0600

    Added acme challenge dir

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require('./src/routes/routes');
-
+const https = require('https');
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+const ssl = {
+    key: fs.readFileSync('clave-privada.pem'),
+    cert: fs.readFileSync('certificado.pem'),
+};
+
+const server = https.createServer(ssl, app);
+
+
+app.use(express.static(path.join(__dirname, 'dist')));
 app.use('/event', routes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
+// server.listen(PORT, () => {
+//     console.log(`Servidor escuchando en https://localhost:${port}`);
+// });
